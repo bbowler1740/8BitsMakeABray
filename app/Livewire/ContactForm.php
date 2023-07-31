4593410f95d5eabb06ee25897dcf8d3a7a63ba4d
bbowler1740 <bbowler1740@conestogac.on.ca>
2023-07-31 09:18:30 -0400

    Update various styling. Introduce validation rules to contact form.

--- app/Livewire/ContactForm.php
+++ app/Livewire/ContactForm.php
@@ -2,20 +2,32 @@
 
 namespace App\Livewire;
 
+use Livewire\Attributes\Rule;
 use Livewire\Component;
 
 class ContactForm extends Component
 {
+    #[Rule('required|email', message: 'Please provide a valid e-mail address.', onUpdate: false)]
     public $visitorEmail = '';
-    public $visitorMessage = 'Write your message here...';
+    #[Rule('required', message: 'Please provide an inquiry message.', onUpdate: false)]
+    public $visitorMessage = '';
 
     public function render()
     {
         return view('livewire.contact-form');
     }
 
-    public function save()
+    public function save():void
     {
+        if($this->validate()) {
+            //TODO: Create an email object based on form data. Send to personal email address.
+            $this->resetForm();
+        }
+    }
 
+    public function resetForm():void
+    {
+        $this->visitorEmail = '';
+        $this->visitorMessage = '';
     }
 }
--- resources/views/partials/contact-information.blade.php
+++ resources/views/partials/contact-information.blade.php
@@ -1,12 +1,12 @@
 <div class="scale-100 p-6 bg-white dark:bg-gray-800/50 dark:bg-gradient-to-bl from-gray-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-2xl shadow-gray-500/20 dark:shadow-none">
     <div class="grid grid-rows-1 grid-cols-5 gap-6">
         <div  class="col-start-2 col-end-6 ml-12">
-            <h2 class="text-xl font-semibold text-gray-900 dark:text-white">Contact Information</h2>
+            <h2 class="text-xl font-semibold text-gray-900 dark:text-white">Social Media</h2>
 
-            <ul class="mt-1 list-disc list-inside text-gray-500 dark:text-gray-400 text-sm leading-relaxed">
-                <li>Twitter</li>
-                <li>LinkedIn</li>
-                <li>GitHub</li>
+            <ul class="mt-2 list-disc list-inside text-gray-500 dark:text-gray-400 text-sm leading-relaxed">
+                <li><a href="https://www.twitter.com/8BitsMakeABray" target="_blank" class="motion-safe:hover:scale-[1.10]">Twitter/X</a></li>
+                <li><a href="https://www.linkedin.com/in/brayden-bowler/" target="_blank" class="motion-safe:hover:scale-[1.10]">LinkedIn</a></li>
+                <li><a href="https://www.github.com/bbowler1740" target="_blank" class="motion-safe:hover:scale-[1.10]">GitHub</a></li>
             </ul>
         </div>
     </div>
