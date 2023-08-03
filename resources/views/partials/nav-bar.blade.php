7648e134c799e9c14a858fbd8ca074343a1690c8
bbowler1740 <bbowler1740@conestogac.on.ca>
2023-08-03 13:34:17 -0400

    convert disabled class to tailwind class

--- resources/views/partials/nav-bar.blade.php
+++ resources/views/partials/nav-bar.blade.php
@@ -1,10 +1,3 @@
-<style>
-    .disabled-link {
-        pointer-events: none;
-        cursor:default;
-    }
-</style>
-
 <div class="p-6 rounded-lg bg-white dark:bg-gray-800/50 dark:bg-gradient-to-bl from-gray-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 shadow-2xl shadow-gray-500/20 dark:shadow-none flex">
     <div class="flex gap-6 mt-6 text-xl font-semibold text-gray-900 dark:text-white">
         {{--$routes derived from NavBarComposer--}}
