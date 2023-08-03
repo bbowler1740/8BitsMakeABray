c0f59063d7129e2cd968725a387af3c2f5e00862
bbowler1740 <bbowler1740@conestogac.on.ca>
2023-08-03 16:56:10 -0400

    Various styling changes on 'About Me' and 'Contact Information' pages.

--- resources/views/partials/about-me-card.blade.php
+++ resources/views/partials/about-me-card.blade.php
@@ -1,21 +1,21 @@
 <div class="scale-100 p-6 bg-white dark:bg-gray-800/50 dark:bg-gradient-to-bl from-gray-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-2xl shadow-gray-500/20 dark:shadow-none">
     <div class="grid grid-rows-1 grid-cols-5 gap-6">
-
-        <div class="col-start-1 rounded-full overflow-hidden h-60 w-60 ml-3 mt-8 justify-center border-4 border-red-100 dark:border-red-800/20">
-            <img src="{{asset('images/about-me-photo.PNG')}}" alt="Brayden Bowler Headshot" class="h-60 w-60">
+        <div class="col-start-1 col-end-2 rounded-full h-64 w-64 overflow-hidden justify-center border-4 border-gray-500 dark:border-gray-400/20">
+            <img src="{{asset('images/about-me-photo.PNG')}}" alt="Brayden Bowler Headshot" class="object-scale-down">
         </div>
 
-        <div  class="col-start-2 col-end-6 ml-12">
-            <h2 class="mt-6 text-xl font-semibold text-gray-900 dark:text-white">About Me</h2>
+        <div  class="col-start-2 col-end-6 ml-10">
+            <h2 class="text-xl font-semibold text-gray-900 dark:text-white">About Me</h2>
 
-            <p class="mt-2 text-gray-500 dark:text-gray-400 text-sm leading-relaxed">
+            <p class="text-gray-500 dark:text-gray-400 text-sm leading-relaxed">
                 Hey! {{"\u{1F44B}"}}<br>
                 My name is Brayden Bowler. {{"\u{1F471}"}} <br>
-                I'm currently working with <a href="https://vehikl.com" target="_blank" class="underline text-blue-800 dark:text-white-800">Vehikl.com</a> to provide professional web development services to our clients. {{"\u{1F5A5}"}} <br>
-                Academically, I'm studying Computer Science at <a href="https://www.conestogac.on.ca/fulltime/bachelor-of-computer-science-honours" target="_blank" class="underline text-blue-800 dark:text-white-800"> Conestoga College</a>. {{"\u{1F4DA}"}} <br>
+                I'm currently working with <a href="https://vehikl.com" target="_blank" class="underline text-blue-800 dark:text-white-800 motion-safe:hover:scale-[1.10]">Vehikl.com</a> to provide professional web development services to our clients. {{"\u{1F5A5}"}} <br>
+                Academically, I'm studying Computer Science at <a href="https://www.conestogac.on.ca/fulltime/bachelor-of-computer-science-honours" target="_blank" class="underline text-blue-800 dark:text-white-800 motion-safe:hover:scale-[1.10]"> Conestoga College</a>. {{"\u{1F4DA}"}} <br>
                 Outside of work and school I enjoy:
             </p>
-            <ul class="mt-1 list-disc list-inside text-gray-500 dark:text-gray-400 text-sm leading-relaxed">
+
+            <ul class="list-disc list-inside text-gray-500 dark:text-gray-400 text-sm leading-relaxed">
                 <li>Golf {{"\u{1F3CC}"}}</li>
                 <li>Rock climbing & Hiking {{"\u{1F9D7}"}}</li>
                 <li>Fitness {{"\u{1F3CB}"}}</li>
--- resources/views/partials/contact-information.blade.php
+++ resources/views/partials/contact-information.blade.php
@@ -1,6 +1,6 @@
 <div class="scale-100 p-6 bg-white dark:bg-gray-800/50 dark:bg-gradient-to-bl from-gray-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-2xl shadow-gray-500/20 dark:shadow-none">
     <div class="grid grid-rows-1 grid-cols-5 gap-6">
-        <div  class="col-start-2 col-end-6 ml-12">
+        <div  class="col-start-1 col-end-5">
             <h2 class="text-xl font-semibold text-gray-900 dark:text-white">Social Media</h2>
 
             <ul class="mt-2 list-disc list-inside text-gray-500 dark:text-gray-400 text-sm leading-relaxed">
