3d44ccb552acaa3684d6697a4b6e0735763db655
bbowler1740 <bbowler1740@conestogac.on.ca>
2023-08-04 13:27:13 -0400

    Styling changes

--- resources/views/partials/about-me-card.blade.php
+++ resources/views/partials/about-me-card.blade.php
@@ -1,6 +1,6 @@
 <div class="scale-100 p-6 bg-white dark:bg-gray-800/50 dark:bg-gradient-to-bl from-gray-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-2xl shadow-gray-500/20 dark:shadow-none">
     <div class="grid grid-rows-1 grid-cols-5 gap-6">
-        <div class="col-start-1 col-end-2 rounded-full h-64 w-64 overflow-hidden justify-center border-4 border-gray-500 dark:border-gray-400/20">
+        <div class="col-start-1 col-end-2 rounded-full h-64 w-64 overflow-hidden justify-center border-4 border-gray-500 dark:border-gray-400/60">
             <img src="{{asset('images/about-me-photo.PNG')}}" alt="Brayden Bowler Headshot" class="object-scale-down">
         </div>
 
