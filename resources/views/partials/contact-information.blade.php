75269622d1993af727ee1a992b3e74bf4c4935ac
bbowler1740 <bbowler1740@conestogac.on.ca>
2023-09-20 08:54:43 -0400

    Update social media text link to platform icons.

--- resources/views/partials/contact-information.blade.php
+++ resources/views/partials/contact-information.blade.php
@@ -1,14 +1,19 @@
-<div class="scale-100 p-6 bg-white dark:bg-gray-800/50 dark:bg-gradient-to-bl from-gray-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-2xl shadow-gray-500/20 dark:shadow-none">
-    <div class="grid grid-cols-5">
-        <div class="col-start-1 col-end-5">
-            <h2 class="text-xl font-semibold text-gray-900 dark:text-white">Social Media</h2>
-
-            <div class="text-gray-500 dark:text-gray-400 text-sm">
-                <p class="motion-safe:hover:scale-[1.005]"><a href="https://www.twitter.com/8BitsMakeABray" target="_blank">Twitter/X</a></p>
-                <p class="motion-safe:hover:scale-[1.005]"><a href="https://www.linkedin.com/in/brayden-bowler/" target="_blank">LinkedIn</a></p>
-                <p class="motion-safe:hover:scale-[1.005]"><a href="https://www.github.com/bbowler1740" target="_blank">GitHub</a></p>
-            </div>
-        </div>
+<div class="scale-100 p-6 bg-white dark:bg-gray-800/50 dark:bg-gradient-to-bl from-gray-700/50
+            via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg
+            shadow-2xl shadow-gray-500/20 dark:shadow-none">
+    <h2 class="text-xl font-semibold text-gray-900 dark:text-white">Social Media</h2>
+    <div class="text-gray-500 dark:text-gray-400 text-sm">
+        <a href="https://www.twitter.com/8BitsMakeABray" target="_blank">
+            <img class="w-16 h-16 motion-safe:hover:scale-[1.500]"
+                 src="{{asset('images/x-social-media-logo-icon.png')}}" alt="X Logo">
+        </a>
+        <a href="https://www.linkedin.com/in/brayden-bowler/" target="_blank">
+            <img class="w-16 h-16 motion-safe:hover:scale-[1.500]"
+                 src="{{asset('images/linkedin.png')}}" alt="LinkedIn Logo">
+        </a>
+        <a href="https://www.github.com/bbowler1740" target="_blank">
+            <img class="w-16 h-16" src="{{asset('images/github-logo.png')}}" alt="GitHub Logo">
+        </a>
     </div>
 </div>
 
